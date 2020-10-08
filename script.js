545e23f9f0c57fba982d98acd21a90c1133a534a
Adam Laskowski <adam.laskowski1127@gmail.com>
2020-10-08 14:52:56 -0500

    JQuery to display content on HTML, edited HTML

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ var category = "";
 var cocktailDBURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
 var searchBox = $('#searchBox');
 var searchButton = $('.search');
-searchButton.on("click", function () {
+var foodTitleEl = $('#foodTitle');
+var drinkTitleEl = $('#drinkTitle');
+var foodImageEl = $('#foodImage');
+var drinkImageEl = $('#drinkImage');
+var drinkInstructionsEl = $('#drinkInstuctions');
+
+
+searchButton.on("change", function () {
     let category = $('.search option:selected').text();
     console.log(category);
     var mealDBURL = "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + category;
@@ -16,6 +23,22 @@ searchButton.on("click", function () {
             method: "GET"
         }).then(function (cocktailDB) {
             // console.log(mealDB.meals);
+
+            var meal = mealDB.meals[0].strMeal;
+            foodTitleEl.text(meal)
+
+            var mealImage = mealDB.meals[0].strMealThumb;
+            foodImageEl.attr("src", mealImage)
+
+            var drink = cocktailDB.drinks[0].strDrink;
+            drinkTitleEl.text(drink)
+
+            var drinkImage = cocktailDB.drinks[0].strDrinkThumb;
+            drinkImageEl.attr("src", drinkImage)
+
+            var drinkInstructions = cocktailDB.drinks[0].strInstructions;
+            drinkInstructionsEl.text(drinkInstructions)
+
             console.log(cocktailDB);
             if (category == "1") {
                 // this for loop give us back 4 random results for "beef"
