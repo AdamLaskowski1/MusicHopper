06088014afc5e58666886d3d5296b796e85ba64d
Adam Laskowski <adam.laskowski1127@gmail.com>
2020-10-09 00:26:52 -0500

    Pushing to main

--- script.js
+++ script.js
@@ -7,11 +7,29 @@ var drinkTitleEl = $('#drinkTitle');
 var foodImageEl = $('#foodImage');
 var drinkImageEl = $('#drinkImage');
 var drinkInstructionsEl = $('#drinkInstuctions');
-
-
-searchButton.on("change", function () {
-    let category = $('.search option:selected').text();
-    console.log(category);
+var searches = []
+function loadSearches(){
+    if (localStorage.getItem("searches")){
+        searches=JSON.parse(localStorage.getItem("searches"))
+        $("#pastSearches").empty()
+        var count=0
+         for (let i = searches.length -1; i >=  0 && count <= 5 ; i--, count++) {
+            if(count <5){
+                var a = $(`<a href="#!" class="collection-item">${searches[i]}</a>`)
+                   $("#pastSearches").prepend(a)
+            } 
+         }
+         $(".collection-item").on("click",function(){
+           var category= $(this).text()
+           loadFD(category)
+         })
+    }
+}
+loadSearches()
+if (searches.length > 1){
+    loadFD(searches[searches.length-1])
+}
+function loadFD(category){
     var mealDBURL = "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + category;
     $.ajax({
         url: mealDBURL,
@@ -22,57 +40,37 @@ searchButton.on("change", function () {
             url: cocktailDBURL,
             method: "GET"
         }).then(function (cocktailDB) {
+            if(searches.includes(category)=== false){
+                searches.push(category)
+            localStorage.setItem("searches", JSON.stringify(searches) )
+            loadSearches()
+            }
             // console.log(mealDB.meals);
             var randomMealNumber = Math.floor(Math.random() * mealDB.meals.length);
-
             var randomMeal = mealDB.meals[randomMealNumber];
-
             console.log("this is the meal:", randomMeal);
-
             foodTitleEl.text(randomMeal.strMeal);
-
             var mealImage = randomMeal.strMealThumb;
             console.log("mealImage:", mealImage);
             foodImageEl.attr("src", mealImage)
-
-
-
             var randomNumberDrink = Math.floor(Math.random() * cocktailDB.drinks.length);
-
             var randomDrink = cocktailDB.drinks[randomNumberDrink];
-
-
-
-
-
             var drink = randomDrink.strDrink;
             drinkTitleEl.text(drink);
-
             var drinkImage = randomDrink.strDrinkThumb;
             drinkImageEl.attr("src", drinkImage)
-
             var drinkInstructions = randomDrink.strInstructions;
             drinkInstructionsEl.text(drinkInstructions)
             console.log(randomDrink);
-
             // for() {
-
             // }
-
             // console.log("meal db", mealDB);
-
-
-
             // console.log("random number:" + randomNumber);
-
-
-
-
-
-
-
-
-
         });
     });
+}
+searchButton.on("change", function () {
+    let category = $('.search option:selected').text();
+    console.log(category);
+   loadFD(category)
 });
